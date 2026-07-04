3565a16d258433acdb901ebdbff01843a99b5512
sba25035-oss <sba25035@student.cct.ie>
2026-07-04 13:37:11 +0100

    color code updated
    capital G added

--- Bicycle_counter.py
+++ Bicycle_counter.py
@@ -187,7 +187,7 @@ fig = px.bar(
     x="Year", 
     y="Fremont Bridge Sidewalks, south of N 34th St Total",
     title="Yearly Total Cyclist Crossings",
-    color_continuous_scale="greens"
+    color_continuous_scale="Greens"
 )
 
 st.plotly_chart(fig, use_container_width=True) 
