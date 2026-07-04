13cc530344ecd0d2538a38694a9c4601df4bf93c
sba25035-oss <sba25035@student.cct.ie>
2026-07-04 12:58:24 +0100

    missing comma
    added

--- Bicycle_counter.py
+++ Bicycle_counter.py
@@ -181,11 +181,11 @@ fig = px.bar(
     yearly_totals,
     x="Year", 
     y="Fremont Bridge Sidewalks, south of N 34th St Total",
-    title="Yearly Total Cyclist Crossings"
+    title="Yearly Total Cyclist Crossings",
     color_continuous_scale="greens"
 )
 
-st.plotly_chart(fig, use_container_width=True) )
+st.plotly_chart(fig, use_container_width=True) 
 
 
 
