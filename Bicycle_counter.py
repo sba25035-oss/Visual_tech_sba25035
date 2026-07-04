442a8dff046ad5006335adc1ddebcce2c6ab0023
sba25035-oss <sba25035@student.cct.ie>
2026-07-04 13:26:53 +0100

    Update Bicycle_counter.py

--- Bicycle_counter.py
+++ Bicycle_counter.py
@@ -170,6 +170,9 @@ st.area_chart(sidewalk_totals)
 # Creating Plotly charts
 
 yearly_totals = (
+        data[DATE_COLUMN] = pd.to_datetime(data["Date"])
+        data["Year"] = data["Date"].dt.year
+       yearly_totals = (
         data.groupby ("Year")[
              "Fremont Bridge Sidewalks, south of N 34th St Total",
         ]
